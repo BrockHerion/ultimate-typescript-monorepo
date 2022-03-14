b6cd88884f04fdfe612dac76489ab4bf1d7a7df7
Brock Herion <brockheriondev@gmail.com>
2022-03-14 16:50:50 -0500

    Built all projects with pnpm

--- apps/todo-next/next.config.js
+++ apps/todo-next/next.config.js
@@ -1,6 +1,25 @@
+const { join } = require('path');
+const workspace = join(__dirname, '..');
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	webpack: (config, options) => {
+    config.module = {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          include: [workspace],
+          exclude: /node_modules/,
+          use: options.defaultLoaders.babel,
+        },
+      ],
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
--- apps/todo-next/src/pages/_app.tsx
+++ apps/todo-next/src/pages/_app.tsx
@@ -1,8 +1,53 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import type { AppProps } from 'next/app';
+import { AppRouter } from '@todo/api/src/routers/_app';
+import { withTRPC } from '@trpc/next';
+import { transformer } from '../utils/trpc';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+function getBaseUrl() {
+  if (process.browser) {
+    return '';
+  }
+  // // reference for vercel.com
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export default withTRPC<AppRouter>({
+  config() {
+    const url = `${getBaseUrl()}/api/trpc`;
+
+    return {
+      links: [
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === 'development' ||
+            (opts.direction === 'down' && opts.result instanceof Error),
+        }),
+        httpBatchLink({
+          url,
+        }),
+      ],
+
+      transformer,
+    };
+  },
+  ssr: true,
+  responseMeta({ clientErrors }) {
+    if (clientErrors.length) {
+      return {
+        status: clientErrors[0].data?.httpStatus ?? 500,
+      };
+    }
+    return {};
+  },
+})(MyApp);
